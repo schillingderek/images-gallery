f95c250ec6e1f24cf7d22d7da53d3332f081ab1b
Derek Schilling <schilling.derek@gmail.com>
2023-09-27 16:34:00 -0500

    accessing images api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,22 @@ import Header from './components/Header';
 import Search from './components/Search';
 import React from 'react';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 const App = () => {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
