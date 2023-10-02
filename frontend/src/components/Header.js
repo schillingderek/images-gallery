5e67af213aad6df02712c0307a0d0ae17c61fb64
Derek Schilling <schilling.derek@gmail.com>
2023-10-01 22:06:43 -0500

    add welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
-import { ReactComponent as Logo } from '../images/logo.svg'
+import React from "react";
+import { Container, Navbar } from "react-bootstrap";
+import { ReactComponent as Logo } from "../images/logo.svg";
 
 const Header = ({ title }) => {
-    return (
-        <Navbar bg="dark" data-bs-theme="dark">
-            <Container>
-                <Logo style={{ maxWidth: '12rem', maxHeight: '3rem' }}/>
-            </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Logo alt={title} style={{ maxWidth: "12rem", maxHeight: "3rem" }} />
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
