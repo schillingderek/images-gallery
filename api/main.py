7285429815c7ed2025cdcd6a52b811f9d41db7eb
Derek Schilling <schilling.derek@gmail.com>
2023-10-03 11:11:55 -0500

    move secrets to python env

--- api/main.py
+++ api/main.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL="https://api.unsplash.com/photos/random"
+UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY")
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    params = {
+        "query": word
+        }
+    headers = {
+        "Authorization": "Client-ID " + UNSPLASH_KEY, 
+        "Accept-Version": "v1"
+        }
+    response = requests.get(url=UNSPLASH_URL, params=params, headers=headers)
+
+    data = response.json()
+    return data
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
